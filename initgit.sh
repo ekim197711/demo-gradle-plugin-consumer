62b91d9ceb095bfde158be2adf421b5937f329ea
Mike Møller Nielsen <ekim197711@gmail.com>
2019-09-13 21:58:53 +0200

    gradle consumer

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# demo-gradle-plugin-consumer" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/demo-gradle-plugin-consumer.git
+git push -u origin master
